9650afeb0b11677b15f9e7a0f3c80b4dfe6b98fc
Kensaku Komatsu <kensaku.komatsu@gmail.com>
2025-09-18 07:06:08 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { Row, Card, Col, Typography } from 'antd';
+import { route1, route2 } from './config';
 import './App.css';
+
+const { Title } = Typography;
+const { Meta } = Card;
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title level={2}>Anchor KOBE Access Map</Title>
       </header>
+      <div className='main'>
+        <div className='route1'>
+          <Title level={3}>新幹線 新神戸駅より</Title>
+          新神戸駅から地下鉄で三宮駅へ。東口の改札を出てください。
+          <Row gutter={[16, 16]}>
+            {route1.map(({ filename, description }, idx) => (
+              <Col xs={24} sm={12} md={8} key={idx}>
+                <Card hoverable>
+                  <img src={`/assets/photos/${filename}`} alt={filename} width={320} />
+                  <Meta title={idx + 1} description={description} />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+
+        <div className='route2'>
+          <Title level={3}>神戸空港 / TPAC 会場より</Title>
+          神戸空港もしくはTPAC会場より、 ポートライナーで三宮駅へ。改札を出てください。
+          <Row gutter={[16, 16]}>
+            {route2.map(({ filename, description }, idx) => (
+              <Col span={8} key={idx}>
+                <Card hoverable>
+                  <img src={`/assets/photos/${filename}`} alt={filename} width={320} />
+                  <Meta title={idx + 1} description={description} />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,29 @@
+export const route1 = [
+  { idx: 0, filename: 'スライド1.jpeg', description: '東口改札を出たら、真っ直ぐ進みます。' },
+  { idx: 1, filename: 'スライド2.jpeg', description: '右奥に進んでください。' },
+  { idx: 2, filename: 'スライド3.jpeg', description: 'エレベーターホールが見えます。' },
+  { idx: 3, filename: 'スライド4.jpeg', description: 'エレベーターで 3F へ上がります。' },
+  { idx: 4, filename: 'スライド5.jpeg', description: 'エレベーターを降りたら左に向かい、道なりに進みます。' },
+  { idx: 5, filename: 'スライド6.jpeg', description: '喫茶店横にエレベーターホール入り口へ進んでください。' },
+  { idx: 6, filename: 'スライド7.jpeg', description: 'エレベーターで 15F へ上がります。' },
+  { idx: 7, filename: 'スライド8.jpeg', description: 'エレベーターを降りたら、真っ直ぐ会場にお入りください。' },
+  { idx: 8, filename: 'スライド9.jpeg', description: '入って右に曲がると会場です。' },
+]
+
+export const route2 = [
+  { idx: 0, filename: 'スライド11.jpeg', description: '改札を出たら右に曲がり、まっすぐ進みます。' },
+  { idx: 1, filename: 'スライド12.jpeg', description: 'エスカレーターで下におります。' },
+  { idx: 2, filename: 'スライド13.jpeg', description: 'エスカレーターを降りたら左に曲がり、 真っ直ぐ進みます。' },
+  { idx: 3, filename: 'スライド14.jpeg', description: '左前方の階段を上ります。' },
+  { idx: 4, filename: 'スライド15.jpeg', description: '横断歩道を真っ直ぐ進みます。' },
+  { idx: 5, filename: 'スライド16.jpeg', description: '階段を降りてください。' },
+  { idx: 6, filename: 'スライド17.jpeg', description: '階段を降りたら、右手に進みます。' },
+  { idx: 7, filename: 'スライド18.jpeg', description: '道なりに真っ直ぐ進みます。' },
+  { idx: 8, filename: 'スライド19.jpeg', description: '右手エスカレーター方向へ進みます。' },
+  { idx: 9, filename: 'スライド20.jpeg', description: 'エスカレーターで 3F へ上がります。' },
+  { idx: 10, filename: 'スライド21.jpeg', description: 'エスカレーターを降りたら、正面喫茶店横のエレベーターホール入り口へ進んでください。' },
+  { idx: 11, filename: 'スライド7.jpeg', description: 'エレベーターで 15F へ上がります。' },
+  { idx: 12, filename: 'スライド8.jpeg', description: 'エレベーターを降りたら、真っ直ぐ会場にお入りください。' },
+  { idx: 13, filename: 'スライド9.jpeg', description: '入って右に曲がると会場です。' },
+]
+
