bd63a0fac76418f02968b1ed22730d3b8b7bb69c
Kensaku Komatsu <kensaku.komatsu@gmail.com>
2025-10-27 18:01:35 +0900

    updated layout, change lang etc.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,134 @@
-import { Row, Card, Col, Typography } from 'antd';
-import { route1, route2 } from './config';
+import { useEffect, useState, useCallback } from 'react';
+import { Button, Row, Card, Col, Divider, Tabs, Typography } from 'antd';
+import { maintitle, mapTitle, routeTitle, routeDescription, route1, route2 } from './config';
 import './App.css';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 const { Title } = Typography;
 const { Meta } = Card;
 
+function Route( props ) {
+  const { lang, name } = props;
+  const route = name === 'route1' ? route1 : route2;
 
+
+  return (
+    <div className='route'>
+      <Title level={2}>{routeTitle[name][lang]}</Title>
+      <p>{routeDescription[name][lang]}</p>
+      <Row gutter={[16, 16]}>
+        {route.map(({ filename, description }, idx) => (
+          <Col xs={24} sm={24} md={12} lg={12} xl={8} key={idx}>
+            <Card hoverable>
+              <div style={{ textAlign: 'center', width: '100%' }}>
+                <img src={`/assets/photos/${filename}`} alt={filename} style={{ maxWidth: '100%', height: 'auto' }} />
+              </div>
+              <Meta title={idx + 1} description={description[lang]} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
+}
+
+function Map( props ) {
+  const { lang } = props;
+  return (
+    <div className='map'>
+      <Card title={mapTitle[lang]} style={{ marginBottom: '20px' }}>
+        <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.450004571602!2d135.19348349999999!3d34.6938281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60008fc6d2fec343%3A0x79309402198363c0!2zQU5DSE9SIEtPQkXvvIjjgqLjg7Pjgqvjg7znpZ7miLjvvIk!5e0!3m2!1sen!2sjp!4v1761543879003!5m2!1sen!2sjp" 
+          width="100%" 
+          height="450" 
+          style={{ border: 0 }}
+          allowFullScreen="" 
+          loading="lazy" 
+          title="Anchor KOBE Map"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </Card>
+    </div>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Title level={2}>Anchor KOBE Access Map</Title>
-      </header>
-      <div className='main'>
-        <div className='route1'>
-          <Title level={3}>新幹線 新神戸駅より</Title>
-          新神戸駅から地下鉄で三宮駅へ。東口の改札を出てください。
-          <Row gutter={[16, 16]}>
-            {route1.map(({ filename, description }, idx) => (
-              <Col xs={24} sm={12} md={8} key={idx}>
-                <Card hoverable>
-                  <img src={`/assets/photos/${filename}`} alt={filename} width={320} />
-                  <Meta title={idx + 1} description={description} />
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
+  const [ _lang, setLang ] = useState('');
+  const [ _routeIdx, setRouteIdx ] = useState(1);
 
-        <div className='route2'>
-          <Title level={3}>神戸空港 / TPAC 会場より</Title>
-          神戸空港もしくはTPAC会場より、 ポートライナーで三宮駅へ。改札を出てください。
-          <Row gutter={[16, 16]}>
-            {route2.map(({ filename, description }, idx) => (
-              <Col xs={24} sm={12} md={8} key={idx}>
-                <Card hoverable>
-                  <img src={`/assets/photos/${filename}`} alt={filename} width={320} />
-                  <Meta title={idx + 1} description={description} />
-                </Card>
-              </Col>
-            ))}
-          </Row>
+  useEffect(() => {
+    let lang, routeIdx;
+    if( window.location.hash.includes('_lang=en')) {
+      lang = 'en'
+    } else if( window.location.hash.includes('_lang=jp') ) {
+      lang = 'jp'
+    } else  {
+      lang = navigator.language.startsWith('en') ? 'en' : 'jp';
+    }
+
+    if( window.location.hash.includes('_routeIdx=1')) {
+      routeIdx = 1
+    } else if( window.location.hash.includes('_routeIdx=2') ) {
+      routeIdx = 2
+    } else  {
+      routeIdx = 1;
+    }
+    setLang(lang);
+    setRouteIdx(routeIdx);
+    document.title = maintitle[lang];
+  }, [])
+
+  /*
+   * change Hash according to lang and routeIdx
+   */
+  useEffect(() => {
+    if( !_lang || !_routeIdx ) return;
+
+    const hash = `#_lang=${_lang}&_routeIdx=${_routeIdx}`;
+    window.location.hash = hash;
+  }, [ _lang, _routeIdx])
+  
+
+  const changeLang = useCallback(( lang ) => {
+    setLang( lang );
+    document.title = maintitle[lang];
+  }, [])
+
+  const changeRoute = useCallback(( idx ) => {
+    setRouteIdx( idx );
+  }, [])
+
+  return (
+    <div className="App background-with-alpha">
+      <div className="container">
+        { ( _lang === 'en' || _lang === 'jp' ) ? (
+        <div className='main'>
+          <div className='lang-select'>
+            <div>
+              <Button onClick={ () => changeLang('jp') } disabled={ _lang === 'jp' } type="default" ><span className="fi fi-jp" />日本語</Button>
+              <span> | </span>
+              <Button onClick={ () => changeLang('en') } disabled={ _lang === 'en' } type="default"><span className='fi fi-gb' />English</Button>
+            </div>
+          </div>
+          <header className="App-header">
+            <Title level={1}>{maintitle[_lang]}</Title>
+          </header>
+          <Tabs 
+            activeKey={_routeIdx.toString()}
+            items={[
+              { label: routeTitle['route1'][_lang], key: '1', children: <Route lang={_lang} name='route1' /> },
+              { label: routeTitle['route2'][_lang], key: '2', children: <Route lang={_lang} name='route2' /> },
+            ]} 
+            onChange={changeRoute}
+          />
+          <Divider />
+          <Map lang={_lang} />
+          <Divider />
+          <footer>
+            <p>&copy; Kensaku KOMATSU, 2025</p>
+          </footer>
         </div>
+      ): <div />}
       </div>
     </div>
   );
--- src/config.js
+++ src/config.js
@@ -1,29 +1,130 @@
+export const maintitle = {
+  'en': 'Directions to Anchor KOBE',
+  'jp': 'アンカー神戸への行き方',
+}
+
+export const routeTitle = {
+  'route1': {
+    'en': 'From Kobe Airport / TPAC Venue',
+    'jp': '神戸空港 / TPAC 会場より',
+  },
+  'route2': {
+    'en': 'From Shinkansen, Shin-Kobe Station',
+    'jp': '新幹線( 新神戸駅 )より',
+  },
+}
+
+export const routeDescription = {
+  'route1': {
+    'en': 'From Kobe Airport or TPAC venue, take the Port Liner to Sannomiya Station. Exit from the ticket gate.',
+    'jp': '神戸空港もしくはTPAC会場より、 ポートライナーで三宮駅へ。改札を出てください。',
+  },
+  'route2': {
+    'en': 'From Shin-Kobe Station, take the subway to Sannomiya Station. Exit from the East Exit ticket gate.',
+    'jp': '新神戸駅から地下鉄で三宮駅へ。東口改札を出てください。',
+  },
+}
+
+export const mapTitle = {
+  'en': 'Map to Anchor KOBE',
+  'jp': 'アンカー神戸への地図',
+}
+
+
 export const route1 = [
-  { idx: 0, filename: 'スライド1.jpeg', description: '東口改札を出たら、真っ直ぐ進みます。' },
-  { idx: 1, filename: 'スライド2.jpeg', description: '右奥に進んでください。' },
-  { idx: 2, filename: 'スライド3.jpeg', description: 'エレベーターホールが見えます。' },
-  { idx: 3, filename: 'スライド4.jpeg', description: 'エレベーターで 3F へ上がります。' },
-  { idx: 4, filename: 'スライド5.jpeg', description: 'エレベーターを降りたら左に向かい、道なりに進みます。' },
-  { idx: 5, filename: 'スライド6.jpeg', description: '喫茶店横にエレベーターホール入り口へ進んでください。' },
-  { idx: 6, filename: 'スライド7.jpeg', description: 'エレベーターで 15F へ上がります。' },
-  { idx: 7, filename: 'スライド8.jpeg', description: 'エレベーターを降りたら、真っ直ぐ会場にお入りください。' },
-  { idx: 8, filename: 'スライド9.jpeg', description: '入って右に曲がると会場です。' },
+  { idx: 0, filename: 'スライド11.jpeg', description: {
+    'en': 'After exiting the ticket gate, turn right and go straight ahead.',
+    'jp': '改札を出たら右に曲がり、まっすぐ進みます。' },
+  },
+  { idx: 1, filename: 'スライド12.jpeg', description: {
+    'en': 'Take the escalator down.',
+    'jp': 'エスカレーターで下におります。' },
+  },
+  { idx: 2, filename: 'スライド13.jpeg', description: {
+    'en': 'After getting off the escalator, turn left and go straight ahead.',
+    'jp': 'エスカレーターを降りたら左に曲がり、真っ直ぐ進みます。' },
+  },
+  { idx: 3, filename: 'スライド14.jpeg', description: {
+    'en': 'Go up the stairs on the left front.',
+    'jp': '左前方の階段を上ります。' },
+  },
+  { idx: 4, filename: 'スライド15.jpeg', description: {
+    'en': 'Go straight across the crosswalk.',
+    'jp': '横断歩道を真っ直ぐ進みます。' },
+  },
+  { idx: 5, filename: 'スライド16.jpeg', description: {
+    'en': 'Please go down the stairs.',
+    'jp': '階段を降りてください。' },
+  },
+  { idx: 6, filename: 'スライド17.jpeg', description: {
+    'en': 'After going down the stairs, go to the right.',
+    'jp': '階段を降りたら、右手に進みます。' },
+  },
+  { idx: 7, filename: 'スライド18.jpeg', description: {
+    'en': 'Go straight along the road.',
+    'jp': '道なりに真っ直ぐ進みます。' },
+  },
+  { idx: 8, filename: 'スライド19.jpeg', description: {
+    'en': 'Proceed to the right-hand escalator.',
+    'jp': '右手エスカレーター方向へ進みます。' },
+  },
+  { idx: 9, filename: 'スライド20.jpeg', description: {
+    'en': 'Go up to the 3rd floor by escalator.',
+    'jp': 'エスカレーターで 3F へ上がります。' },
+  },
+  { idx: 10, filename: 'スライド21.jpeg', description: {
+    'en': 'After getting off the escalator, proceed to the entrance of the elevator hall next to the café.',
+    'jp': 'エスカレーターを降りたら、正面喫茶店横のエレベーターホール入り口へ進んでください。' },
+  },
+  { idx: 11, filename: 'スライド7.jpeg', description: {
+    'en': 'Take the elevator to the 15th floor.',
+    'jp': 'エレベーターで 15F へ上がります。' },
+  },
+  { idx: 12, filename: 'スライド8.jpeg', description: {
+    'en': 'After getting off the elevator, go straight into the venue.',
+    'jp': 'エレベーターを降りたら、真っ直ぐ会場にお入りください。' },
+  },
+  { idx: 13, filename: 'スライド9.jpeg', description: {
+    'en': 'Turn right after entering to find the venue.',
+    'jp': '入って右に曲がると会場です。' },
+  },
 ]
 
 export const route2 = [
-  { idx: 0, filename: 'スライド11.jpeg', description: '改札を出たら右に曲がり、まっすぐ進みます。' },
-  { idx: 1, filename: 'スライド12.jpeg', description: 'エスカレーターで下におります。' },
-  { idx: 2, filename: 'スライド13.jpeg', description: 'エスカレーターを降りたら左に曲がり、 真っ直ぐ進みます。' },
-  { idx: 3, filename: 'スライド14.jpeg', description: '左前方の階段を上ります。' },
-  { idx: 4, filename: 'スライド15.jpeg', description: '横断歩道を真っ直ぐ進みます。' },
-  { idx: 5, filename: 'スライド16.jpeg', description: '階段を降りてください。' },
-  { idx: 6, filename: 'スライド17.jpeg', description: '階段を降りたら、右手に進みます。' },
-  { idx: 7, filename: 'スライド18.jpeg', description: '道なりに真っ直ぐ進みます。' },
-  { idx: 8, filename: 'スライド19.jpeg', description: '右手エスカレーター方向へ進みます。' },
-  { idx: 9, filename: 'スライド20.jpeg', description: 'エスカレーターで 3F へ上がります。' },
-  { idx: 10, filename: 'スライド21.jpeg', description: 'エスカレーターを降りたら、正面喫茶店横のエレベーターホール入り口へ進んでください。' },
-  { idx: 11, filename: 'スライド7.jpeg', description: 'エレベーターで 15F へ上がります。' },
-  { idx: 12, filename: 'スライド8.jpeg', description: 'エレベーターを降りたら、真っ直ぐ会場にお入りください。' },
-  { idx: 13, filename: 'スライド9.jpeg', description: '入って右に曲がると会場です。' },
+  { idx: 0, filename: 'スライド1.jpeg', description: {
+    'en': 'After exiting the East Exit ticket gate, go straight ahead.',
+    'jp': '東口改札を出たら、真っ直ぐ進みます。' }
+  },
+  { idx: 1, filename: 'スライド2.jpeg', description: {
+    'en': 'Please proceed to the right rear.',
+    'jp': '右奥に進んでください。' },
+  },
+  { idx: 2, filename: 'スライド3.jpeg', description: {
+    'en': 'You will see the elevator hall.',
+    'jp': 'エレベーターホールが見えます。' },
+  },
+  { idx: 3, filename: 'スライド4.jpeg', description: {
+    'en': 'Take the elevator to the 3rd floor.',
+    'jp': 'エレベーターで 3F へ上がります。' },
+  },
+  { idx: 4, filename: 'スライド5.jpeg', description: {
+    'en': 'After getting off the elevator, turn left and follow the path.',
+    'jp': 'エレベーターを降りたら左に向かい、道なりに進みます。' },
+  },
+  { idx: 5, filename: 'スライド6.jpeg', description: {
+    'en': 'Please proceed to the entrance of the elevator hall next to the café.',
+    'jp': '喫茶店横のエレベーターホール入り口へ進んでください。' },
+  },
+  { idx: 6, filename: 'スライド7.jpeg', description: {
+    'en': 'Take the elevator to the 15th floor.',
+    'jp': 'エレベーターで 15F へ上がります。' },
+  },
+  { idx: 7, filename: 'スライド8.jpeg', description: {
+    'en': 'After getting off the elevator, go straight into the venue.',
+    'jp': 'エレベーターを降りたら、真っ直ぐ会場にお入りください。' },
+  },
+  { idx: 8, filename: 'スライド9.jpeg', description: {
+    'en': 'Turn right after entering to find the venue.',
+    'jp': '入って右に曲がると会場です。' },
+  },
 ]
-
